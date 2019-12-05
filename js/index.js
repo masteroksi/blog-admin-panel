64007c2d877c2e7d185936279f834ca74f1f291a
oksi <masteroksi@gmail.com>
2019-12-05 22:21:14 +0200

    add js

---
+++ js/index.js
@@ -0,0 +1,21 @@
+const filmInfo = {
+    name: "Carnival Row",
+    year: 2019,
+    month: 8,
+    day: 30,
+    duration: "56 min",
+    country: "USA",
+    "18+": false,
+    actors: [
+        "Orlando Bloom",
+        "Cara Delevingne",
+        "Simon McBurney",
+        "Tamzin Merchant",
+        "David Gyasi",
+        "Andrew Gower",
+        "Karla Crome",
+        "Arty Froushan",
+        "Indira Varma",
+        "Jared Harris",
+    ]
+};
