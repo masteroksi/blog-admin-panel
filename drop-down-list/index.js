9e612d0de5bb382f356c8c94086fea17ad791e04
oksi <masteroksi@gmail.com>
2020-02-14 18:13:28 +0200

    list in list

--- drop-down-list/index.js
+++ drop-down-list/index.js
@@ -1,24 +1,63 @@
-const buttons = document.querySelectorAll('.js-btn');
-document
-    .querySelectorAll('.js-drop-down-list')
-    .forEach((el) => el.style.display = 'none');
+(function () {
+    const buttons = document.querySelectorAll('.js-btn');
 
-function toggleList(list) {
-    list.style.display = list.style.display === 'block' ? 'none' : 'block'
-}
+    document
+        .querySelectorAll('.js-drop-down-list')
+        // .forEach((el) => el.style.display = 'none');
+        .forEach((el) => el.classList.add('hidden'));
 
-buttons.forEach((button) => {
-    button.addEventListener('click', (ev) => {
-        const clickedBtn = ev.target;
-        const siblingList = clickedBtn.nextElementSibling;
-        const childList = clickedBtn.querySelector('.js-drop-down-list');
-        if (childList !== null) {
-            toggleList(childList);
-        } else if (siblingList.className === 'js-drop-down-list') {
-            toggleList(siblingList);
-        }
+    function toggleList(list) {
+        // list.style.display = list.style.display === 'block' ? 'none' : 'block';
+        list.classList.toggle('hidden');
+    }
+
+    buttons.forEach((button) => {
+        button.addEventListener('click', (ev) => {
+            ev.stopPropagation();
+            const clickedBtn = ev.target;
+            const siblingList = clickedBtn.nextElementSibling;
+            const childList = clickedBtn.querySelector('.js-drop-down-list');
+            if (childList) {
+                toggleList(childList);
+            } else if (siblingList.className.includes('js-drop-down-list')) {
+                toggleList(siblingList);
+            }
+        });
     });
-});
+})();
+
+
+// const button = document.querySelector('.js-show-list');
+// const lists = document.querySelector('.js-list');
+// lists.classList.add('hidden');
+// button.addEventListener('click', function () {
+//     lists.classList.toggle('hidden');
+//     showLine(lists, 1000);
+// });
+//
+// document.querySelectorAll('.js-show-sublist')
+//     .forEach((el) => el.classList.add('hidden'));
+//
+// function showLine(el, delay) {
+//     const children = el.querySelectorAll(':scope > .js-show-sublist');
+//
+//     if (children.length === 0) {
+//         const subList = el.querySelector(':scope > .js-sublist');
+//         if (subList == null) {
+//             return;
+//         }
+//         showLine(subList, delay);
+//         return;
+//     }
+//     setTimeout(() => {
+//         document.querySelector('.js-time').innerHTML = delay;
+//         children.forEach((el) => el.classList.remove('hidden'));
+//         children.forEach((el) => showLine(el, delay + 1000));
+//     }, delay);
+// }
+
+
+
 
 
 
