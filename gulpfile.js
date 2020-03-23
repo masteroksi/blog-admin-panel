378d917a77e64fea83d177ee10d9bb7236cc7476
oksi <masteroksi@gmail.com>
2020-03-23 21:50:03 +0200

    commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,62 @@
 const gulp = require('gulp'),
- concat = require('gulp-concat'),
- sass = require('gulp-sass');
+    concat = require('gulp-concat'),
+    sass = require('gulp-sass'),
+    ejs = require('gulp-ejs'),
+    rename = require('gulp-rename'),
+    connect = require('gulp-connect');
+
+function server() {
+    return connect.server({
+        root: 'dist',
+        livereload: true,
+    });
+}
+
+function template() {
+    return gulp.src('./src/template/*.ejs')
+        .pipe(ejs())
+        .pipe(rename({extname: '.html'}))
+        .pipe(gulp.dest('./dist'))
+        .pipe(connect.reload());
+}
 
 function js() {
-    return gulp.src('./src/js/**/*.js').pipe(concat('app.js')).pipe(gulp.dest('./dist/js'))
+    return gulp.src('./src/js/**/*.js')
+        .pipe(concat('app.js'))
+        .pipe(gulp.dest('./dist/js'))
+        .pipe(connect.reload());
+
 }
 
 function scss() {
-    return gulp.src('./src/css/**/*.scss').pipe(sass()).pipe(gulp.dest('./dist/css'));
+    return gulp.src('./src/css/**/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./dist/css'))
+        .pipe(connect.reload());
+
 }
 
 function copyImages() {
-    return gulp.src('./src/images/**').pipe(gulp.dest('./dist/images'))
+    return gulp.src('./src/images/**')
+        .pipe(gulp.dest('./dist/images'));
 }
 
 function copyFonts() {
-    return gulp.src('./src/fonts/**').pipe(gulp.dest('./dist/fonts'))
+    return gulp.src('./src/fonts/**')
+        .pipe(gulp.dest('./dist/fonts'));
 }
 
 function watch() {
+    copyImages();
+    copyFonts();
+    scss();
+    js();
+    template();
+
     gulp.watch('./src/css/**/*.scss', scss);
     gulp.watch('./src/js/**/*.js', js);
+    gulp.watch('./src/template/**/*.ejs', template);
+    server();
 }
 
 // gulp.task('sass:watch', function () {
@@ -32,10 +68,13 @@ exports.scss = scss;
 exports.copyImages = copyImages;
 exports.copyFonts = copyFonts;
 exports.copy = gulp.series(copyImages, copyFonts);
+exports.tmp = template;
+exports.s = server;
+
 
 exports.watch = watch;
 exports.build = gulp.series(
     gulp.parallel(js, scss),
     copyImages,
-    copyFonts
+    copyFonts,
 );
--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,69 @@
-console.log('hello ');
+// document.querySelector(".js--show-nav-dashboards").onclick = function() {
+//     open();
+//     showNavbar()
+//
+// };
+// function open() {
+//     document.querySelector('.js-menu-items').classList.toggle('show')
+//
+// }
+//
+// function showDashboards() {
+//     const buttons = document.querySelector('.js--show-nav-dashboards');
+//     document
+//         .querySelector('.js-menu-items')
+//         // .forEach((el) => el.style.display = 'none');
+//         .forEach((el) => el.classList.add('hidden'));
+// }
+//
+//
+
+
+const button = document.querySelector('.js-show-nav-dashboards');
+const Dashboards = document.querySelector('.js-nav-dashboards');
+
+button.addEventListener('click', function (ev) {
+    ev.stopPropagation();
+    Dashboards.classList.toggle('nav-dashboards_show');
+});
+
+document.body.addEventListener('click', function () {
+    Dashboards.classList.remove('nav-dashboards_show');
+});
+
+
+
+
+const dashboard = document.querySelector('.js-dashboard-col');
+
+// const  content = document.querySelector('.js-main-content');
+
+if (dashboard) {
+    let startCursorX;
+    let startCursorY;
+    let startX;
+    let startY;
+
+    dashboard.addEventListener('dragstart',function () {
+        startCursorX = event.pageX;
+        startCursorY = event.pageY;
+        startX = dashboard.style.marginLeft.replace('px','')*1;
+        startY = dashboard.style.marginTop.replace('px','')*1;
+    });
+    dashboard.addEventListener('dragover',function() {
+        event.preventDefault();
+    });
+
+    dashboard.addEventListener('dragend',function() {
+        dashboard.style.position = 'absolute';
+        dashboard.style.marginLeft = startX + event.pageX - startCursorX; //позиция элемента + позиция курсора - позиция курсоа в начале перетаскивания
+        dashboard.style.marginTop = startY + event.pageY - startCursorY; // Так же как и в предыдущем случае, только по другой оси
+
+    });
+}
+
+
+
+
+
 
