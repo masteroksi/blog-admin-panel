b0809b33e4a85658528531b338b0e33135c09154
oksi <masteroksi@gmail.com>
2020-03-24 19:14:38 +0200

    fix js

--- docs/js/app.js
+++ docs/js/app.js
@@ -80,13 +80,13 @@ if (dragula) {
 
 
 
-const imgContainers = document.querySelectorAll('.js-svg');
-imgContainers.forEach((element) => {
-    const url = element.dataset.src;
-    fetch(url).then(res => res.text()).then((res) => {
-        element.innerHTML = res;
-    });
-});
+// const imgContainers = document.querySelectorAll('.js-svg');
+// imgContainers.forEach((element) => {
+//     const url = element.dataset.src;
+//     fetch(url).then(res => res.text()).then((res) => {
+//         element.innerHTML = res;
+//     });
+// });
 
 //  span cl="js-svg" data-src="dist/images/f.svd"
 
